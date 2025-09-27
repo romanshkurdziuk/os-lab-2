6aa38b90272727e7598c414757e95a38529131a0
Roman Shkurdyuk <romansh0106@gmail.com>
2025-09-27 12:49:17 +0300

    refactor: Decompose main logic into separate functions

--- main.cpp
+++ main.cpp
@@ -1,103 +1,139 @@
 #include <iostream>
 #include <vector>
 #include <windows.h>
+#include <optional>
 
 
 using namespace std;
 
 struct dataStruct
 {
-    vector<int>* arr;
+    vector<int>* vec;
     int maxValue;
     int minValue;
     double averageValue;
 };
 
-DWORD WINAPI findMinMax (LPVOID lpParameter)
+struct MinMax
 {
-    dataStruct* data = (dataStruct*)(lpParameter);
-    int size = data->arr->size();
-    data->minValue = data->arr->at(0);
-    data->maxValue = data->arr->at(0);
-    for (int i = 1; i < size; ++i)
+    int min;
+    int max;
+};
+
+struct Avarage
+{
+    double avarage;
+};
+
+MinMax FunctionfindMinMax(const vector<int> &vec)
+{
+    if (vec.empty())
     {
-        if (data->arr->at(i) < data->minValue)
+        return {0, 0};
+    }
+    int size = vec.size();
+    MinMax result = {vec[0], vec[0]};
+    for (int i = 0; i < size; ++i)
+    {
+        if (vec[i] < result.min)
         {
-            data->minValue = data->arr->at(i);
+            result.min = vec[i];
         }
         Sleep(7);
-        if (data->arr->at(i) > data->maxValue)
+        if (vec[i] > result.max)
         {
-            data->maxValue = data->arr->at(i);
+            result.max = vec[i];
         }
         Sleep(7);
     }
+    return result;
+}
+
+
+DWORD WINAPI findMinMax (LPVOID lpParameter)
+{
+    dataStruct* data = (dataStruct*)(lpParameter);
+    MinMax minMax = FunctionfindMinMax(*data->vec);
+    data->minValue = minMax.min;
+    data->maxValue = minMax.max;
     return 0;
+}
+
+Avarage FunctionfindAvarage(const vector<int> &vec)
+{
+    if (vec.empty())
+    {
+        return;
+    }
+    int size = vec.size();
+    double sum = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        sum += vec[i];
+        Sleep(12);
+    }
+    Avarage result = {sum / size};
+    return result;
 }
 
 DWORD WINAPI findAvarage (LPVOID lpParameter)
 {
     dataStruct* data = (dataStruct*)(lpParameter);
-    int size = data->arr->size();
-    double sum = 0;
-    for (int i = 0; i < size; ++i)
-    {
-        sum += data->arr->at(i);
-        Sleep(12);
-    }
-    double avarage = sum / size;
-    data->averageValue = avarage;
+    Avarage avarage = FunctionfindAvarage(*data->vec);
+    data->averageValue = avarage.avarage;
     return 0;
 }
 
-
-
-using namespace std;
-int main()
+optional<vector <int>> initializeArrayFromInput()
 {
-    cout << "Enter array size: ";
+    cout << "Enter size of array: ";
     int size;
-    cin>>size;
-    if(size <= 0)
+    cin >>size;
+    if (size <= 0)
     {
-        cout << "Error: Array size must be positive... " << endl;
-        return 1;
+        cout << "ERROR: Array size must be positive... " <<endl;
+        return nullopt;
     }
-    vector<int> array(size);
+    vector<int> vec(size);
     cout << "Enter " << size << " array elements" << endl;
     for (int i = 0; i < size; ++i)
     {
         cout << "[" << i << "] ";
-        cin >> array[i];
+        cin >> vec[i];
     }
 
     for (int i = 0; i < size; ++i)
     {
-        cout << array[i] << " ";
+        cout << vec[i] << " ";
     }
     cout << endl;
-    dataStruct data;
-    data.arr = &array;
+    return vec;
+}
 
+void CalculateResult()
+{
+    dataStruct data;
+    auto optionalArray = initializeArrayFromInput();
+    if (!optionalArray)
+    {
+        cout << "ERROR: Array initialization failed..." << endl;
+        return;
+    }
+    vector<int> vec = *optionalArray;
+    data.vec = &vec;
+    int size = data.vec->size();
 
     HANDLE hMinMaxThread = CreateThread(NULL, 0, findMinMax, &data, 0, NULL);
     HANDLE hAvarageThread = CreateThread(NULL, 0, findAvarage, &data, 0, NULL);
 
-    if (hMinMaxThread == NULL || hAvarageThread == NULL)
-    {
-        cout << "Error: Failed to create threads..." << endl;
-        return 1;
-    }
-    WaitForSingleObject(hMinMaxThread, INFINITE);\
-    cout << "DEBUG: MinMax thread has finished waiting." << endl; // <--- МАЯЧОК 4
+    WaitForSingleObject(hMinMaxThread, INFINITE);
+    cout << "DEBUG: MinMax thread has finished waiting." << endl;
     WaitForSingleObject(hAvarageThread, INFINITE);
-    cout << "DEBUG: Average thread has finished waiting." << endl; // <--- МАЯЧОК 5
+    cout << "DEBUG: Average thread has finished waiting." << endl;
     cout << "ALL threads finished... " << endl;
     
     CloseHandle(hMinMaxThread);
     CloseHandle(hAvarageThread);
-
-    cout << "Main thread continues..." << endl;
 
     cout << "Result from threeds:" <<endl;
     cout << "Min element: " << data.minValue << endl;
@@ -106,29 +142,39 @@ int main()
 
     int minIndex = -1;
     int maxIndex = -1;
-
     for (int i = 0; i < size; ++i)
     {
-        if (array[i] == data.minValue && minIndex == -1)
+        if(data.vec->at(i) == data.minValue && minIndex == -1)
         {
             minIndex = i;
         }
-        if (array[i] == data.maxValue && maxIndex == -1)
+        if(data.vec->at(i) == data.maxValue && maxIndex == -1)
         {
             maxIndex = i;
         }
-
     }
     if (maxIndex != -1 && minIndex != -1)
     {
-        array[minIndex] = static_cast<int>(data.averageValue);
-        array[maxIndex] = static_cast<int>(data.averageValue);
+        data.vec->at(minIndex) = static_cast<int>(data.averageValue);
+        data.vec->at(maxIndex) = static_cast<int>(data.averageValue);
     }
 
-    for (int val : array)
+    for (int val : *data.vec)
     {
         cout << val << " ";
     }
     cout << endl;
+
+}
+   
+
+
+
+
+
+using namespace std;
+int main()
+{   
+    CalculateResult();
     return 0;
 }
